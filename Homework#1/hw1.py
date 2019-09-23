38e1d2ccc4a09301c41055b928a0bfaf8f6508e9
hejie <cuihejie331771@gmail.com>
2019-09-23 18:16:41 -0400

    Add comments

--- Homework#1/hw1.py
+++ Homework#1/hw1.py
@@ -1,92 +1,73 @@
+'''
+@Description: Class Ridge and ForwardStagewise, CS534-Machine Learning Hw1 
+@Author: Hejie Cui
+@Date: 2019-09-19 10:54:23
+@LastEditTime: 2019-09-23 18:04:34
+'''
 # Please do not use other libraries except for numpy
+
 import numpy as np
-from sklearn.datasets import load_boston
-from sklearn.linear_model import Ridge
 
 
 class Ridge:
+    """Class for Ridge Regression with Prior Coefficients."""
 
     def __init__(self):
         self.intercept = 0
         self.coef = None
 
-    def loss_func(self, y, X, coef, coef_prior, lmbd, intercept):
-        return(np.sum(np.square(y - intercept - np.dot(X, coef))) + lmbd * np.sum(np.square(coef - coef_prior)))
-
     def fit(self, X, y, coef_prior=None, lmbd=1.0):
+        """
+        Parameters
+        ----------
+        X : ndarray
+            The input data
+        y : ndarray
+            The target data
+        coef_prior : ndarray, optional
+            Prior knowledge, which will be reflected in the penalty term of the loss function
+        lmbd : float, optional
+            Regularization strength, which has the same meaning as the alpha in SKlearn regression but with 
+            a scale difference. That means we need to multiply the lmbd with the sample numbers in the fit 
+            function, that is n, if we want to compare with passing the same value of alpha to the ridge 
+            regression of sklearn.
+            For example, if we passed the lmbd=1.0 to this fit function and alpha=1.0 to the ridge regression 
+            of sklearn, we need to multiply the lmbd with the sample numbers of X (That is n) in the fit function 
+            before figuring out the closed form solution.
+            By doing so, I can get the exactly the same values of intercept and self.coef values compared with sklearn.
+        """
+
         n, m = X.shape
         self.coef = np.zeros(m)
         if coef_prior.any() == None:
             coef_prior = np.zeros(m)
 
-        X = np.c_[np.ones(n), X]
-        coef_prior = np.insert(coef_prior, 0, 0, axis=0)
-
         # a) normalize X
         x_mu = np.average(X, axis=0)
         x_sigma = np.std(X, axis=0)
-        for i in range(1, m + 1):
+        for i in range(m):
             X[:, i] = (X[:, i] - x_mu[i]) / x_sigma[i]
-        # print("X after normalize: ", X)
 
         # b) adjust coef_prior according to the normalization parameters
         for i in range(m):
             coef_prior[i] = coef_prior[i] * x_sigma[i]
 
         # c) get coefficients
-        I = np.eye(m+1)
-        I[0][0] = 0
+        # First, we transform y to have 0 mean. If data are first centered about 0, then favoring small
+        # intercept not so worrisome. Using the loss function, we can get the closed form solution, which
+        # is shown in the calculation formula of self.coef
+        I = np.eye(m)
+        centered_y = y - np.mean(y)
+        # the reason why we need to multiply lmbd with n is explained in the comments of fit function's
+        lmbd *= n
         self.coef = np.dot(np.linalg.inv(np.dot(X.T, X) + lmbd*I),
-                           (lmbd * coef_prior + np.dot(X.T, y)))
-        self.intercept = self.coef[0]
-        print("self.intercept before denormalize: ", self.intercept)
-        print("self.coef before denormalize: ", self.coef[1:])
-
-        # print("self.coef before normalized: ", self.coef)
-        # print("self.intercept before normalized: ", self.intercept)
-        # current_coef = np.zeros(m)
-        # current_intercept = np.zeros(n)
-        # threshold_coef = 1e-4
-        # threshold_intercept = 1e-4
-        # coef_difference = np.inf
-        # intercept_difference = np.inf
-
-        # while coef_difference > threshold_coef:
-        #     new_intercept = y - np.dot(X, current_coef)
-        #     intercept_difference = np.mean(
-        #         np.abs(new_intercept - current_intercept))
-
-        #     XTX = np.dot(X.T, X)
-        #     lmbdI = lmbd * np.eye(m)
-        #     lmbdCoef_prior = lmbd * coef_prior
-        #     XTY = np.dot(X.T, y)
-        #     XTIntercept = np.dot(X.T, new_intercept)
-
-        #     new_coef = np.dot(np.linalg.inv(XTX + lmbdI),
-        #                       (lmbdCoef_prior + XTY - XTIntercept))
-        #     coef_difference = np.mean(np.abs(new_coef - current_coef))
-
-        #     current_intercept = new_intercept
-        #     current_coef = new_coef
-
-        # self.intercept = current_intercept
-        # self.coef = current_coef
+                           (lmbd * coef_prior + np.dot(X.T, centered_y)))
 
         # d) adjust coefficients for de-normalized X
-        for i in range(1, m+1):
+        self.intercept = np.mean(y)
+        for i in range(m):
             self.intercept -= self.coef[i] / x_sigma[i] * x_mu[i]
             self.coef[i] = self.coef[i] / x_sigma[i]
-
-        data = load_boston()
-        X = data.data
-        y = data.target
-        self.coef = self.coef[1:]
-        coef_prior = coef_prior[1:]
-        # X = X[:, 1:]
-        loss = self.loss_func(y, X, self.coef, coef_prior,
-                              lmbd, self.intercept)
-        loss /= n
-        print("loss: ", loss)
 
         return 0
 
@@ -95,12 +76,27 @@ class Ridge:
 
 
 class ForwardStagewise:
+    """Class for Incremental Forward-Stagewise with Cannot-Link Constraints."""
 
     def __init__(self):
         self.intercept = 0
         self.path = []
 
     def fit(self, X, y, cannot_link=[], epsilon=1e-2, max_iter=1000):
+        """
+        Parameters
+        ----------
+        X : ndarray
+            The input data
+        y : ndarray
+            The target data
+        cannot_link : list of list, optional
+            The groups in which at most one of the features in the same group should be active
+        epsilon : float, optional
+            The updated coefficient
+        max_iter: int, optional
+            The maximum interation number
+        """
 
         # a) normalize X
         n, m = X.shape
@@ -113,8 +109,10 @@ class ForwardStagewise:
         # b-2) implement cannot-link constraints
         nsteps = 1000
         beta = np.zeros(m)
+        y_copy = y
         y = y - np.mean(y)
 
+        # a dictionary recording whether the key column is still be updating
         column_is_updating = {k: v for (k, v) in zip(range(m), np.ones(m))}
         path = []
         path.append(np.zeros(m))
@@ -122,17 +120,22 @@ class ForwardStagewise:
         for s in range(nsteps):
             r = y - np.dot(X, beta)
             mse_min, j_best, gamma_best = np.inf, 0, 0
+            # filter the updating columns
             updating_columns = [i for i, is_updating in column_is_updating.items()
                                 if is_updating == 1]
+            # use mse to evaluate which is the best column to choose
             for j in updating_columns:
                 gamma_j = np.dot(X[:, j], r) / np.dot(X[:, j], X[:, j])
                 mse = np.mean(np.square(r - gamma_j * X[:, j]))
                 if mse < mse_min:
                     mse_min, j_best, gamma_best = mse, j, gamma_j
 
+            # find the group index in the cannot_link which the j_best belong to
             group_index = [i for i, group_members in enumerate(cannot_link)
                            if j_best in group_members]
 
+            # if the j_best belongs to a group in the cannot_link, then all the other columns
+            # in this group will no longer be updated
             if len(group_index):
                 for i in cannot_link[group_index[0]]:
                     column_is_updating[i] = 0
@@ -141,26 +144,28 @@ class ForwardStagewise:
 
             if np.abs(gamma_best) > 0:
                 beta[j_best] += gamma_best * epsilon
+                # append the beta for denomalized X to path list
                 path.append(beta.tolist())
-        print("cannot link beta before denormalize: ", beta)
 
         # c) adjust coefficients for de-normalized X
-        self.intercept = np.mean(y)
+        self.intercept = np.mean(y_copy)
         for i in range(m):
             self.intercept -= beta[i] / x_sigma[i] * x_mu[i]
             beta[i] = beta[i] / x_sigma[i]
-        print("cannot link beta after denormalize: ", beta)
-        print("self.intercept after denormalize: ", self.intercept)
 
         # d) construct the "path" numpy array
-        #     path: l-by-m array,
+        #     path: (l+1)-by-m array,
         #               where l is the total number of iterations
         #               m is the number of features in X.
         #               The first row, path[0,:], should be all zeros.
-        path = path[:1000]
+
+        # transform the path list into numpy array and adjust the beta values in path for the denormalized X
         self.path = np.array(path)
+        for j in range(m):
+            self.path[:, j] = self.path[:, j] / x_sigma[j]
 
         return 0
 
     def get_coef_path(self):
+        # return the intercept for denormalized X and each line in path is fitted for the denormalized X
         return self.intercept, self.path
--- Homework#1/test_hw1.py
+++ Homework#1/test_hw1.py
@@ -1,3 +1,9 @@
+'''
+@Description: Test Script for Hw1
+@Author: Hejie Cui
+@Date: 2019-09-19 15:31:02
+@LastEditTime: 2019-09-23 18:02:52
+'''
 import numpy as np
 from sklearn.datasets import load_boston
 from sklearn import linear_model
@@ -5,66 +11,70 @@ from hw1 import Ridge
 from hw1 import ForwardStagewise
 from sklearn.preprocessing import scale
 
-######################## Test Ridge Regression #########################
-# data = load_boston()
-# X = data.data
-# y = data.target
-# n, m = X.shape
-# print("n: {}, m: {}".format(n, m))
-# coef_prior = np.zeros(m)
-# ridge = Ridge()
-# ridge.fit(X, y, coef_prior=coef_prior, lmbd=1)
-# intercept, coef = ridge.get_coef()
-# print("intercept after denormalize:: {}, coef:{} ".format(intercept, coef))
+######################## Test Ridge Regression with Prior Coefficients #########################
+# ridge regression with prior coefficients
+data = load_boston()
+X = data.data
+y = data.target
+n, m = X.shape
 
-# data = load_boston()
-# X = data.data
-# y = data.target
-# # reg_sk = linear_model.Ridge(alpha=1, normalize=True, solver='cholesky')
-# reg_sk = linear_model.Ridge(alpha=1, solver='cholesky')
-# reg_sk.fit(X, y)
-# intercept_sk = reg_sk.intercept_
-# coef_sk = reg_sk.coef_
-# print("intercept_sk:{}, coef_sk:{}".format(intercept_sk, coef_sk))
+coef_prior = np.zeros(m)
+ridge = Ridge()
+ridge.fit(X, y, coef_prior=coef_prior, lmbd=1)
+intercept, coef = ridge.get_coef()
+print("ridge regression with prior coefficients: intercept: {}, coef: {} ".format(
+    intercept, coef))
+
+# sklearn ridge regression
+data = load_boston()
+X = data.data
+y = data.target
+reg_sk = linear_model.Ridge(alpha=1, normalize=True, solver='cholesky')
+reg_sk.fit(X, y)
+intercept_sk = reg_sk.intercept_
+coef_sk = reg_sk.coef_
+print("sklearn ridge regression: intercept_sk:{}, coef_sk:{}".format(
+    intercept_sk, coef_sk))
 ##########################################################################
 
-########################### Test Stagewise Regression ####################
+########################### Test Stagewise Regression with Cannot-Link Constraints ####################
+# stagewise regression with cannot-link constraints
 data = load_boston()
 X = data.data
 n, m = X.shape
 y = data.target
-cannot_link = [[0, 1], [2, 3, 4], [5, 6]]
+# cannot_link = [[0, 1], [2, 3, 4], [5, 6]]
+cannot_link = []
 fsw = ForwardStagewise()
 fsw.fit(X, y, cannot_link=cannot_link)
 intercept, path = fsw.get_coef_path()
 # print("cannot link intercept:", intercept)
-# print("cannot link path:", path)
+print("cannot link path[1000]:", path[1000])
 
-# # Forward Stagewise
-# data = load_boston()
-# X = scale(data.data)
-# n, m = X.shape
-# y = data.target
-# columns = data.feature_names.tolist()
+# sklearn forward stagewise
+data = load_boston()
+X = scale(data.data)
+n, m = X.shape
+y = data.target
+columns = data.feature_names.tolist()
 
-# nsteps = 1000
-# delta = 1e-2
-# beta = np.zeros(m)
-# y = y - np.mean(y)
+nsteps = 1000
+delta = 1e-2
+beta = np.zeros(m)
+y = y - np.mean(y)
 
-# path = []
-# for s in range(nsteps):
-#     r = y - np.dot(X, beta)
-#     mse_min, j_best, gamma_best = np.inf, 0, 0
-#     for j in range(m):
-#         gamma_j = np.dot(X[:, j], r)/np.dot(X[:, j], X[:, j])
-#         mse = np.mean(np.square(r - gamma_j * X[:, j]))
-#         if mse < mse_min:
-#             mse_min, j_best, gamma_best = mse, j, gamma_j
-#     if np.abs(gamma_best) > 0:
-#         beta[j_best] += gamma_best * delta
-#         path.append(beta.tolist())
-
+path = []
+for s in range(nsteps):
+    r = y - np.dot(X, beta)
+    mse_min, j_best, gamma_best = np.inf, 0, 0
+    for j in range(m):
+        gamma_j = np.dot(X[:, j], r)/np.dot(X[:, j], X[:, j])
+        mse = np.mean(np.square(r - gamma_j * X[:, j]))
+        if mse < mse_min:
+            mse_min, j_best, gamma_best = mse, j, gamma_j
+    if np.abs(gamma_best) > 0:
+        beta[j_best] += gamma_best * delta
+        path.append(beta.tolist())
 # print("forward stagewise beta", beta)
-# print("forward stagewise path: ", path)
+# print("forward stagewise path[999]: ", path[999])
 ##########################################################################
