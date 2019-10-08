79a53ea52517f741f5797740c9aae9d200a11674
hejie <cuihejie331771@gmail.com>
2019-10-08 15:49:16 -0400

    hw3 init

---
+++ Homework#3/hw3.py
@@ -0,0 +1,9 @@
+'''
+@Description: In User Settings Edit
+@Author: your name
+@Date: 2019-10-08 15:46:41
+@LastEditTime: 2019-10-08 15:49:09
+@LastEditors: Please set LastEditors
+'''
+from sklearn.datasets import load_breast_cancer
+from sklearn.datasets import load_boston
